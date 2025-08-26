2a25a7726eb1420defcc5a7773c0ac59d0ca5ac3
who100am <mirzagavelizade1001@gmail.com>
2025-08-26 22:32:42 +0400

    Update WplaceHelper.user.js

--- WplaceHelper.user.js
+++ WplaceHelper.user.js
@@ -2,14 +2,14 @@
 // @name         Wplace Helper
 // @namespace    BaronTeam
 // @version      1.0
-// @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Wplace Helper" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by NASA.
+// @description  A userscript to automate and/or enhance the user experience on Wplace.live. Make sure to comply with the site's Terms of Service, and rules! This script is not affiliated with Wplace.live in any way, use at your own risk. This script is not affiliated with TamperMonkey. The author of this userscript is not responsible for any damages, issues, loss of data, or punishment that may occur as a result of using this script. This script is provided "as is" under the MPL-2.0 license. The "Wplace Helper" icon is licensed under CC0 1.0 Universal (CC0 1.0) Public Domain Dedication. The image is owned by USA.
 // @author       BaronTeam
 // @license      MPL-2.0
-// @supportURL   https://discord.gg/tpeBPy46hf
-// @homepageURL  https://github.com/SwingTheVine/Wplace-BlueMarble
+// @supportURL   https://github.com/who100am/WplaceHelper/
+// @homepageURL  https://github.com/who100am/WplaceHelper/
 // @icon         https://i.postimg.cc/6p8HbXN6/Illuminati-triangle-eye-copy-32x32.png
-// @updateURL    https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
-// @downloadURL  https://raw.githubusercontent.com/SwingTheVine/Wplace-BlueMarble/main/dist/BlueMarble.user.js
+// @updateURL    https://raw.githubusercontent.com/who100am/WplaceHelper/refs/heads/main/WplaceHelper.user.js
+// @downloadURL  https://raw.githubusercontent.com/who100am/WplaceHelper/refs/heads/main/WplaceHelper.user.js
 // @run-at       document-start
 // @match        *://*.wplace.live/*
 // @grant        GM_getResourceText
